d23ee3b04b22837dec06b0bc41277f37cabb9534
BaoTQ <qbaok62@gmail.com>
2025-01-23 23:40:43 +0700

    feat: add pack select sequence logic

--- src/components/pack-list.tsx
+++ src/components/pack-list.tsx
@@ -1,4 +1,4 @@
-import { animated, useSpring } from "@react-spring/three";
+import { animated, useSpring, useSprings } from "@react-spring/three";
 import { Html } from "@react-three/drei";
 import { useDrag } from "@use-gesture/react";
 import { useState } from "react";
@@ -12,25 +12,14 @@ const SCALE = {
 };
 const INIT_ROTATION = [0, 0, 0];
 
-const PACK_NUMBER = 12;
-const ANGLE_STEP = (2 * Math.PI) / PACK_NUMBER;
+const PACK_COUNT = 12;
+const ANGLE_STEP = (2 * Math.PI) / PACK_COUNT;
 const VELOCITY_INTENSITY = 0.7;
 const CARD_SIZE = {
   WIDTH: 0.35,
   HEIGHT: 0.6,
 };
 
-const packList = Array(PACK_NUMBER)
-  .fill("")
-  .map((_, idx) => {
-    const angle = ANGLE_STEP * idx;
-    const x = RADIUS * Math.sin(angle);
-    const z = RADIUS * Math.cos(angle);
-    return {
-      angle,
-      position: new THREE.Vector3(x, 0, z),
-    };
-  });
 const geometry = new THREE.PlaneGeometry(CARD_SIZE.WIDTH, CARD_SIZE.HEIGHT);
 const material = new THREE.MeshBasicMaterial({
   color: "red",
@@ -42,22 +31,14 @@ const snapToClosest = (angle: number) => {
 };
 
 export const PackList = () => {
+  const [isInit, setIsInit] = useState(true);
   const [activeIndex, setActiveIndex] = useState(0);
   const [isDragging, setIsDragging] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
 
   const [{ rotation, scale }, set] = useSpring(() => ({
-    from: {
-      rotation: [
-        INIT_ROTATION[0],
-        INIT_ROTATION[1] - Math.PI * 3,
-        INIT_ROTATION[2],
-      ],
-      scale: SCALE.INIT,
-    },
-    to: {
-      rotation: INIT_ROTATION,
-      scale: SCALE.INIT,
-    },
+    rotation: INIT_ROTATION,
+    scale: SCALE.INIT,
     config: { mass: 1, tension: 150, friction: 40 },
     onRest: () => {
       // Snap back to the angle where the closest card is centered
@@ -70,14 +51,34 @@ export const PackList = () => {
       });
       const newActiveIndex =
         snappedY >= 0
-          ? (snappedY / ANGLE_STEP) % PACK_NUMBER
-          : Math.abs(
-              (PACK_NUMBER - (-1 * snappedY) / ANGLE_STEP) % PACK_NUMBER
-            );
+          ? (snappedY / ANGLE_STEP) % PACK_COUNT
+          : Math.abs((PACK_COUNT - (-1 * snappedY) / ANGLE_STEP) % PACK_COUNT);
       setActiveIndex(newActiveIndex);
       setIsDragging(false);
     },
   }));
+
+  const [springs, setPacks] = useSprings(PACK_COUNT, (i) => {
+    const angle = ANGLE_STEP * i;
+    const x = RADIUS * Math.sin(angle);
+    const z = RADIUS * Math.cos(angle);
+    return {
+      from: {
+        rotation: [0, angle, 0],
+        position: [x / 10, 0, z / 10],
+      },
+      to: {
+        rotation: [0, angle, 0],
+        position: [x, 0, z],
+      },
+      delay: i * 100,
+      onRest: () => {
+        if (i === PACK_COUNT - 1) {
+          setIsInit(false);
+        }
+      },
+    };
+  });
 
   const bind = useDrag(
     ({ velocity: [vx], direction: [dx], down, tap }) => {
@@ -107,8 +108,26 @@ export const PackList = () => {
     {
       filterTaps: true,
       delay: 300,
+      enabled: !isInit && !isSelected,
     }
   );
+
+  const handlePackClick = () => {
+    setIsSelected(true);
+    setPacks.start((i) => {
+      if (i === activeIndex) {
+        return {};
+      }
+      const prevPosition = springs[i].position.get();
+      return {
+        position: [prevPosition[0], -10, prevPosition[2]],
+        config: {
+          mass: 1,
+          tension: 150,
+        },
+      };
+    });
+  };
 
   return (
     <>
@@ -121,27 +140,26 @@ export const PackList = () => {
       <animated.group
         position={[0, -CARD_SIZE.HEIGHT / 4, 0]}
         scale={scale}
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        rotation={rotation}
+        rotation={rotation as unknown as THREE.Euler}
       >
-        {packList.map((item, idx) => {
-          const isClickable = !isDragging && activeIndex === idx;
+        {springs.map((item, idx) => {
+          const isClickable = !isInit && !isDragging && activeIndex === idx;
           return (
-            <group
+            <animated.group
               key={idx}
-              rotation={[0, item.angle, 0]}
-              position={item.position}
+              rotation={item.rotation as unknown as THREE.Euler}
+              position={item.position as unknown as THREE.Vector3}
             >
               <Pack
                 geometry={geometry}
                 material={material}
                 isClickable={isClickable}
+                onClick={handlePackClick}
               />
               <Html position={[0, 0, 0.1]} occlude="raycast">
                 {idx}
               </Html>
-            </group>
+            </animated.group>
           );
         })}
       </animated.group>
